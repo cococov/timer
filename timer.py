617e667d16f22b47db3317dbf453e3b9f28f336b
cococov <contact@juanlamas.dev>
2023-07-04 16:32:07 -0400

    style: fix indent

--- timer.py
+++ timer.py
@@ -12,17 +12,17 @@ minutes = int(script_params[1])
 seconds = int(script_params[2])
 
 def timer(hours, minutes, seconds):
-    total_seconds = hours * 60 * 60 + minutes * 60 + seconds
+  total_seconds = hours * 60 * 60 + minutes * 60 + seconds
+  now_epoch = time.time()
+  future_epoch = now_epoch + total_seconds
+  while now_epoch < future_epoch:
     now_epoch = time.time()
-    future_epoch = now_epoch + total_seconds
-    while now_epoch < future_epoch:
-        now_epoch = time.time()
-        seconds = int(future_epoch - now_epoch)
-        minutes, seconds = divmod(seconds, 60)
-        hours, minutes = divmod(minutes, 60)
-        print(f'{FULL_SCREEN_ASCII}{HIDE_CURSOR_ASCII}{DELETE_LINE_ASCII}{hours:02d}:{minutes:02d}:{seconds:02d}', end='\r')
-        sys.stdout.flush()
-        time.sleep(1)
+    seconds = int(future_epoch - now_epoch)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    print(f'{FULL_SCREEN_ASCII}{HIDE_CURSOR_ASCII}{DELETE_LINE_ASCII}{hours:02d}:{minutes:02d}:{seconds:02d}', end='\r')
+    sys.stdout.flush()
+    time.sleep(1)
 
 try:
   timer(hours, minutes, seconds)
